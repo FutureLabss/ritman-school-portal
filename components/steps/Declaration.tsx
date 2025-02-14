d7428fc0567a264ddef491c0f1e6c1ad282bbd1e
Ralpmike <ralphmike996@gmail.com>
2025-02-14 12:11:51 +0100

    feature: studen application form

--- components/steps/Declaration.tsx
+++ components/steps/Declaration.tsx
@@ -1,8 +1,70 @@
+import { useState } from "react";
+import Input from "../Input";
+import Button from "../Button";
+import Image from "next/image";
+
 export default function Declaration() {
+  const [declaration, setDeclaration] = useState({
+    declaration: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setDeclaration({ ...declaration, [name]: value });
+  };
   return (
-    <div>
-      <h2 className="text-xl font-bold">Declaration</h2>
-      <p>Declaration form goes here...</p>
+    <div className="py-12 flex flex-col gap-11 max-w-xl">
+      <h2 className="text-2xl font-semibold">Application Declaration</h2>
+
+      <p className="text-sm leading-6">
+        I declare that the information provided by me in connection with the
+        application is true and correct. I undertake to inform Ritman University
+        immediately of any changes in address, phone numbers and any other
+        information by me in this application. I understand that Ritman
+        University reserves the right to vary or reverse any decision regarding
+        admission or enrollment made on the basis of incorrect or incomplete
+        information.
+      </p>
+      <form action="" className="flex flex-col gap-12">
+        <div className="flex gap-12">
+          <label className="text-[#555] text-sm font-semibold">
+            {" "}
+            Declaration <span className="text-secondary">*</span>{" "}
+          </label>
+          <div className="flex items-center gap-2">
+            <Input
+              type="radio"
+              value={declaration.declaration}
+              onChange={handleChange}
+              name="declaration"
+              id="declaration"
+            />
+            <label
+              className="text-sm font-medium text-[#555]"
+              htmlFor="declaration"
+            >
+              I Agree
+            </label>
+          </div>
+        </div>
+        <div className="flex gap-4 pt-6 text-[#666666] flex-col sm:flex-row sm:self-center">
+          <Button
+            label="Submit Form"
+            className="border-[#ccc] py-2 px-4 text-sm text-center border rounded-md"
+          />
+          <Button
+            label="Clear Form"
+            className="border-[#ccc] py-2 px-4 text-sm text-center border rounded-md"
+          />
+          <div className="flex items-center justify-center border rounded-md py-2 px-4 ">
+            <Image src="/print-light.svg" alt="Logo" width={30} height={30} />
+            <Button
+              label="Print Form"
+              className="border-[#ccc] text-sm text-center"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
--- components/steps/Education.tsx
+++ components/steps/Education.tsx
@@ -1,8 +1,139 @@
+import { useState } from "react";
+import Input from "../Input";
+import Dropdown from "../DropDown";
+
+const Qualification = [
+  { value: "SSCE", label: "SSCE" },
+  { value: "OND", label: "OND" },
+  { value: "HND", label: "HND" },
+  { value: "BSc", label: "BSc" },
+  { value: "MSc", label: "MSc" },
+];
+
 export default function Education() {
+  const [formDataTwo, setFormDataTwo] = useState({
+    jambRegNo: "",
+    programmeOfChoice: "",
+    courseOfChoice: "",
+    academicSession: "",
+    institution: "",
+    qualification: "",
+    fromYear: "",
+    toYear: "",
+  });
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setFormDataTwo({ ...formDataTwo, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formDataTwo);
+  };
+
   return (
-    <div>
-      <h2 className="text-xl font-bold">Education</h2>
-      <p>Education details form goes here...</p>
+    <div className="pb-12">
+      <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 w-full sm:grid-cols-2 gap-y-2 sm:gap-y-12 items-center  max-w-sm pt-20 mb-6">
+          <label className="text-sm font-semibold text-[#555]">
+            JAMB/IJMB Reg No<span className="text-secondary">*</span>:
+          </label>
+          <Input
+            type="text"
+            name="jambRegNo"
+            onChange={handleChange}
+            value={formDataTwo.jambRegNo}
+            className="input"
+          />
+          <label className="text-sm font-semibold text-[#555]">
+            Programme of Choice<span className="text-secondary">*</span>
+          </label>
+          <Input
+            type="text"
+            name="programmeOfChoice"
+            onChange={handleChange}
+            value={formDataTwo.programmeOfChoice}
+            className="input"
+          />
+          <label className="text-sm font-semibold text-[#555]">
+            Course of Choice <span className="text-secondary">*</span>{" "}
+          </label>
+          <Input
+            type="text"
+            name="courseOfChoice"
+            onChange={handleChange}
+            value={formDataTwo.courseOfChoice}
+            className="input"
+          />
+          <label className="text-sm font-semibold text-[#555]">
+            Academic Session <span className="text-secondary">*</span>
+          </label>
+          <Input
+            type="text"
+            name="academicSession"
+            onChange={handleChange}
+            value={formDataTwo.academicSession}
+            className="input"
+          />
+          <label className="text-sm font-semibold text-[#555]">
+            Secondary School / Institution / University{" "}
+            <span className="text-secondary">*</span>
+          </label>
+          <Input
+            type="text"
+            name="institution"
+            className="input"
+            onChange={handleChange}
+            value={formDataTwo.institution}
+          />
+
+          <label className="text-sm font-semibold text-[#555]">
+            Qualification <span className="text-secondary">*</span>
+          </label>
+          <Dropdown
+            name="qualification"
+            options={Qualification}
+            value={formDataTwo.qualification}
+            onChange={handleChange}
+            className="input py-1 focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-col gap-y-4 justify-between sm:flex-row max-w-md">
+          <div className="flex items-center gap-4">
+            <label className="text-sm font-semibold text-[#555]">
+              From(Year)<span className="text-secondary">*</span>
+              <Input
+                type="date"
+                name="fromYear"
+                className="input"
+                onChange={handleChange}
+                value={formDataTwo.fromYear}
+              />
+            </label>
+
+            <span className="inline-block border-2 border-black h-2 w-4 mt-6"></span>
+          </div>
+          <div className="flex items-center gap-4">
+            <label className="text-sm font-semibold text-[#555]">
+              To(Year)<span className="text-secondary">*</span>
+              <Input
+                type="date"
+                name="toYear"
+                className="input"
+                onChange={handleChange}
+                value={formDataTwo.toYear}
+              />
+            </label>
+            <span className="inline-block border-2 border-black h-2 w-4 mt-6"></span>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
