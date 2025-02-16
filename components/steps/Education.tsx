2015895708dc2493572c224fe2f254a0d0b6852b
Ralpmike <ralphmike996@gmail.com>
2025-02-16 15:48:02 +0100

    fix: refactor student-form registration

--- components/steps/Education.tsx
+++ components/steps/Education.tsx
@@ -40,99 +40,108 @@ export default function Education() {
   return (
     <div className="pb-12">
       <form onSubmit={handleSubmit}>
-        <div className="grid grid-cols-1 w-full sm:grid-cols-2 gap-y-2 sm:gap-y-12 items-center  max-w-sm pt-20 mb-6">
-          <label className="text-sm font-semibold text-[#555]">
-            JAMB/IJMB Reg No<span className="text-secondary">*</span>:
-          </label>
-          <Input
-            type="text"
-            name="jambRegNo"
-            onChange={handleChange}
-            value={formDataTwo.jambRegNo}
-            className="input"
-          />
-          <label className="text-sm font-semibold text-[#555]">
-            Programme of Choice<span className="text-secondary">*</span>
-          </label>
-          <Input
-            type="text"
-            name="programmeOfChoice"
-            onChange={handleChange}
-            value={formDataTwo.programmeOfChoice}
-            className="input"
-          />
-          <label className="text-sm font-semibold text-[#555]">
-            Course of Choice <span className="text-secondary">*</span>{" "}
-          </label>
-          <Input
-            type="text"
-            name="courseOfChoice"
-            onChange={handleChange}
-            value={formDataTwo.courseOfChoice}
-            className="input"
-          />
-          <label className="text-sm font-semibold text-[#555]">
-            Academic Session <span className="text-secondary">*</span>
-          </label>
-          <Input
-            type="text"
-            name="academicSession"
-            onChange={handleChange}
-            value={formDataTwo.academicSession}
-            className="input"
-          />
-          <label className="text-sm font-semibold text-[#555]">
-            Secondary School / Institution / University{" "}
-            <span className="text-secondary">*</span>
-          </label>
-          <Input
-            type="text"
-            name="institution"
-            className="input"
-            onChange={handleChange}
-            value={formDataTwo.institution}
-          />
+        <div className="grid grid-cols-1 w-full sm:grid-cols-2 gap-4  sm:gap-y-8 items-center  max-w-3xl pt-20 mb-6">
+          <div>
+            <label className="text-sm font-semibold text-[#555]">
+              JAMB/IJMB Reg No<span className="text-secondary">*</span>:
+            </label>
+            <Input
+              type="text"
+              name="jambRegNo"
+              onChange={handleChange}
+              value={formDataTwo.jambRegNo}
+              className="input mt-2"
+            />
+          </div>
+          <div>
+            <label className="text-sm font-semibold text-[#555]">
+              Programme of Choice<span className="text-secondary">*</span>
+            </label>
+            <Input
+              type="text"
+              name="programmeOfChoice"
+              onChange={handleChange}
+              value={formDataTwo.programmeOfChoice}
+              className="input mt-2"
+            />
+          </div>
+          <div>
+            <label className="text-sm font-semibold text-[#555]">
+              Course of Choice <span className="text-secondary">*</span>{" "}
+            </label>
+            <Input
+              type="text"
+              name="courseOfChoice"
+              onChange={handleChange}
+              value={formDataTwo.courseOfChoice}
+              className="input mt-2"
+            />
+          </div>
+          <div>
+            <label className="text-sm font-semibold text-[#555]">
+              Academic Session <span className="text-secondary">*</span>
+            </label>
+            <Input
+              type="text"
+              name="academicSession"
+              onChange={handleChange}
+              value={formDataTwo.academicSession}
+              className="input mt-2"
+            />
+          </div>
+          <div>
+            <label className="text-sm font-semibold text-[#555]">
+              Secondary School / Institution / University{" "}
+              <span className="text-secondary">*</span>
+            </label>
+            <Input
+              type="text"
+              name="institution"
+              className="input mt-2"
+              onChange={handleChange}
+              value={formDataTwo.institution}
+            />
+          </div>
 
-          <label className="text-sm font-semibold text-[#555]">
-            Qualification <span className="text-secondary">*</span>
-          </label>
-          <Dropdown
-            name="qualification"
-            options={Qualification}
-            value={formDataTwo.qualification}
-            onChange={handleChange}
-            className="input py-1 focus:outline-none"
-          />
-        </div>
-        <div className="flex flex-col gap-y-4 justify-between sm:flex-row max-w-md">
-          <div className="flex items-center gap-4">
+          <div>
+            <label className="text-sm font-semibold text-[#555]">
+              Qualification <span className="text-secondary">*</span>
+            </label>
+            <Dropdown
+              name="qualification"
+              options={Qualification}
+              value={formDataTwo.qualification}
+              onChange={handleChange}
+              className="input py-1 mt-2 focus:outline-none"
+            />
+          </div>
+          {/* <div className="flex flex-col gap-y-4 justify-between sm:flex-row max-w-md"> */}
+          <div className="flex flex-col  gap-2">
             <label className="text-sm font-semibold text-[#555]">
               From(Year)<span className="text-secondary">*</span>
-              <Input
-                type="date"
-                name="fromYear"
-                className="input"
-                onChange={handleChange}
-                value={formDataTwo.fromYear}
-              />
             </label>
-
-            <span className="inline-block border-2 border-black h-2 w-4 mt-6"></span>
+            <Input
+              type="date"
+              name="fromYear"
+              className="input"
+              onChange={handleChange}
+              value={formDataTwo.fromYear}
+            />
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex flex-col gap-2">
             <label className="text-sm font-semibold text-[#555]">
               To(Year)<span className="text-secondary">*</span>
-              <Input
-                type="date"
-                name="toYear"
-                className="input"
-                onChange={handleChange}
-                value={formDataTwo.toYear}
-              />
             </label>
-            <span className="inline-block border-2 border-black h-2 w-4 mt-6"></span>
+            <Input
+              type="date"
+              name="toYear"
+              className="input"
+              onChange={handleChange}
+              value={formDataTwo.toYear}
+            />
           </div>
         </div>
+        {/* </div> */}
       </form>
     </div>
   );
--- components/steps/Identification.tsx
+++ components/steps/Identification.tsx
@@ -38,18 +38,18 @@ export default function Identification() {
   };
 
   return (
-    <div className="py-20 max-w-md">
+    <div className="py-20 max-w-3xl">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col space-y-6 sm:space-y-12"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2"
       >
-        <div className="flex flex-col sm:flex-row gap-x-12">
+        <div className="flex flex-col gap-x-12">
           <label htmlFor="" className="mb-2 text-sm font-semibold">
             Nationality <span className="text-secondary">*</span>
           </label>
           <div className="flex flex-col-reverse sm:flex-col">
             <Input
-              className="sm:max-w-[10.625rem] border-[#6F6F6F] border w-full rounded-md"
+              className=" border-[#6F6F6F] border w-full rounded-md"
               type="text"
               name="nationality"
               value={formIdentificationData.nationality}
@@ -59,27 +59,25 @@ export default function Identification() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-y-4 sm:flex-row sm:items-center">
-          <div className="space-y-2">
-            <span className="text-sm font-semibold">Type of ID Document</span>
-            <Dropdown
-              options={typeOfId}
-              name="documentType"
-              value={formIdentificationData.documentType}
-              onChange={handleCahnge}
-              className="input py-[4.8px] focus:outline-none"
-            />
-          </div>
-          <div className="space-y-2">
-            <span className="text-sm font-semibold">ID Number</span>
-            <Input
-              className="input"
-              type="text"
-              name="idNumber"
-              value={formIdentificationData.idNumber}
-              onChange={handleCahnge}
-            />
-          </div>
+        <div className="space-y-2">
+          <span className="text-sm font-semibold">Type of ID Document</span>
+          <Dropdown
+            options={typeOfId}
+            name="documentType"
+            value={formIdentificationData.documentType}
+            onChange={handleCahnge}
+            className="input py-[4.8px] focus:outline-none"
+          />
+        </div>
+        <div className="space-y-2">
+          <span className="text-sm font-semibold">ID Number</span>
+          <Input
+            className="input"
+            type="text"
+            name="idNumber"
+            value={formIdentificationData.idNumber}
+            onChange={handleCahnge}
+          />
         </div>
 
         <div className="flex flex-col">
@@ -94,7 +92,6 @@ export default function Identification() {
               onChange={handleCahnge}
               className="input"
             />
-            <span className="inline-block border-2 border-black h-2 w-4 "></span>
           </div>
           <span className="text-xs text-[#6F6F6F] font-medium">Date</span>
         </div>
@@ -102,7 +99,7 @@ export default function Identification() {
           <label htmlFor="" className="mb-2 text-sm font-semibold">
             Date of Expiry
           </label>
-          <div className="flex gap-1 items-center">
+          <div>
             <Input
               type="date"
               name="expiryDate"
@@ -110,7 +107,6 @@ export default function Identification() {
               onChange={handleCahnge}
               className="input"
             />
-            <span className="inline-block border-2 border-black h-2 w-4"></span>
           </div>
           <span className="text-xs text-[#6F6F6F] font-medium">Date</span>
         </div>
